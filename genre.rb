541c13141fcec934a9f7b777431a04e048d2f623
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-10-10 19:52:55 -0300

    Add music album and genre files

---
+++ genre.rb
@@ -0,0 +1,23 @@
+class Genre
+  attr_reader :name
+
+  def initialize(name, _id = rand(0..1000))
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_genre(self)
+  end
+
+  def self.list_genres(things)
+    genres_names = []
+    things.each do |thing|
+      genres_names << thing.genre.name unless genres_names.include?(thing.genre.name)
+    end
+    genres_names.each_with_index do |name, index|
+      puts "[#{index}] Genre: #{name}"
+    end
+  end
+end
