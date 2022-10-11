15feb1fd592f62938491768004f8d8429a458053
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-10-11 14:20:36 -0300

    Add schema for games, authors, music and sources

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
+-- sources
 CREATE TABLE sources(
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(50),
     PRIMARY KEY(id)
 );
-
+-- movies
 CREATE TABLE movies(
     id INT GENERATED ALWAYS AS IDENTITY,
     publish_date DATE NOT NULL,
@@ -13,8 +14,40 @@ CREATE TABLE movies(
     PRIMARY KEY(id),
     CONSTRAINT fk_sources FOREIGN KEY(sources_id) REFERENCES sources(id)
 );
-
-
-
--- ALTER TABLE movies ADD COLUMN sources_id INT; 
--- ALTER TABLE movies ADD CONSTRAINT fk_sources FOREIGN KEY(sources_id) REFERENCES sources(id);
+-- genres
+CREATE TABLE genres(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    PRIMARY KEY(id)
+);
+-- music albums
+CREATE TABLE music_albums(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN,
+    on_spotify BOOLEAN,
+    genres_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_genres FOREIGN KEY(genres_id) REFERENCES genres(id)
+);
+-- authors
+CREATE TABLE authors(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    PRIMARY KEY(id)
+);
+-- games
+CREATE TABLE games(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN,
+    multiplayer BOOLEAN,
+    last_played_at DATE NOT NULL
+    games_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_authors FOREIGN KEY(games_id) REFERENCES authors(id)
+);
+-- generating indexes for performance
+CREATE INDEX movie_source_idx ON movies(sources_id);
+CREATE INDEX music_genre_idx ON music_albums(genres_id);
+CREATE INDEX game_author_idx ON games(authors_id);
