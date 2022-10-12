8074acc4e884644e218621db16f6e0dcab53d73e
Muhammad Ashraf <forazkar@gmail.com>
2022-10-12 19:42:27 +0500

    Update book.rb

--- book.rb
+++ book.rb
@@ -3,23 +3,23 @@ require_relative 'source'
 require_relative 'author'
 
 class Book < Item
-  attr_accessor :cover_state
-  attr_accessor :publisher
+  attr_accessor :cover_state, :publisher
 
   def initialize(*args, publisher, cover_state)
     super(*args)
     @publisher = publisher
     @cover_state = cover_state
   end
-  
+
   def self.list_books(books)
     books.each_with_index do |books, index|
-      puts "[#{index}] The Book: #{books.cover_state} by #{books.author.first_name} #{books.author.last_name} has been Published by #{books.publisher} on #{books.publish_date}" if books.instance_of? Book
+      if books.instance_of? Book
+        puts "[#{index}] The Book: #{books.cover_state} by #{books.author.first_name} #{books.author.last_name} has been Published by #{books.publisher} on #{books.publish_date}"
+      end
     end
   end
 
   def self.add_book
-    
     print 'Author\'s first name?'
     print "\nAnswer: "
     author_first_name = gets.chomp
@@ -36,7 +36,7 @@ class Book < Item
     print "\nType a publish date [year]: "
     print "\nAnswer: "
     book_date = gets.chomp.to_i
-   
+
     new_book = Book.new(book_date, cover_state, publisher)
     puts "The book '#{cover_state.upcase}' by #{publisher.upcase} was created successfully!"
     new_author = Author.new(author_first_name, author_last_name)
@@ -46,7 +46,7 @@ class Book < Item
   end
 
   def can_be_archived?
-    super || @cover_state==="bad"
+    super || @cover_state == 'bad'
     false
   end
 end
