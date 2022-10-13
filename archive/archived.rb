14b7f6566cc39d7ef9d55a27a66146ad55fa4b28
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-10-13 12:04:41 -0300

    Save changes

--- archive/archived.rb
+++ archive/archived.rb
@@ -5,9 +5,40 @@ class Archived
     things.each do |thing|
       thing.move_to_archive
       if thing.archived
-        puts "[#{index}] #{thing.label.title} - can be archived"
+        puts "#{index}) [#{thing.class}] #{thing.label.title} (ID: #{thing.id}) - can be archived"
         index += 1
       end
     end
+    puts "\nDo you want to archive an item?"
+    puts "1) Yes"
+    puts "2) No"
+    print "Answer: "
+    case_answer(gets.chomp, things)
+  end
+
+  def self.case_answer(ans, things)
+    case ans.downcase
+    when '1'
+      puts archive_an_item(things)
+    when 'yes'
+      puts archive_an_item(things)
+    when '2'
+      puts 'Back to the menu!'
+    when 'no'
+      puts 'Back to the menu!'
+    else
+      puts 'Invalid input!'
+    end
+  end
+
+  def self.archive_an_item(things)
+    puts "\nPlease, select an item by its ID"
+    print "Answer: "
+    item_selected = gets.chomp
+    archived_items = []
+    item_to_be_archived = things.find{|thing| thing.id == item_selected.to_i}
+    archived_items << item_to_be_archived
+    things.delete_if{|thing| thing.id == item_selected.to_i}
+    p archived_items
   end
 end
